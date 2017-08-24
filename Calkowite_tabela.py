f19e9c68caa1f101443748cf3f5f235d7f1f4349
lo1cgsan <lo1cgsan@gmail.com>
2017-08-24 22:30:56 +0200

    Poprawka odczytywania i wstawiania wielu danych do ORM Peewee z pliku csv

--- Calkowite_tabela.py
+++ Calkowite_tabela.py
@@ -5,20 +5,25 @@ from peewee import *
 
 # sprawdzimy czy baza istnieje
 if os.path.exists('calkowite.db'):
-	os.remove('calkowite.db')
+    os.remove('calkowite.db')
 
 # stworzymy instancje bazy uzywanej przez modele
 baza = SqliteDatabase('calkowite.db')
 
 # teraz klasa bazowa z której będziemy czerpac
+
+
 class BazaModel(Model):
-	class Meta:
-		database = baza
+    class Meta:
+        database = baza
 
 # teraz tabela z promieniowaniem bezposrednim
+
+
 class Calkowite(BazaModel):
-	data = CharField(null=False)
-	pomiary = CharField(null=False)
+    data = CharField(null=False)
+    pomiary = CharField(null=False)
+
 
 # nawiązanie połączneia z bazą
 baza.connect()
@@ -26,35 +31,34 @@ baza.connect()
 baza.create_table(Calkowite, True)
 
 #############################################################################
-# odczytamy sobie dane z pliku i dodamy do tupli
-dane = []
 
 
 def pobieraj_dane(plikcsv):
-	with open(plikcsv, "r") as zawartosc:
-		klucze = ('data', 'pomiary')
-		reader = csv.DictReader(zawartosc, klucze, delimiter=',')
+    """odczytamy sobie dane z pliku i dodamy do tupli"""
+    dane = []
+    with open(plikcsv, newline='') as zawartosc:
+        reader = csv.DictReader(zawartosc, delimiter=',')
+        for linia in reader:
+            dane.append(linia)
+    return (dane)  # przeksztalcamy liste na tuple i zwracamy ja
 
-		for linia in reader:
-			dane.append(linia)
-	return (dane)  # przeksztalcamy liste na tuple i zwracamy ja
 
-pobieraj_dane('outputCalkowite.csv')
+dane = pobieraj_dane('outputCalkowite.csv')
+# print(dane)
 #############################################################################
 
 # dodamy wiele danych - rozne terminy, rozne pomiary
 Calkowite.insert_many(dane).execute()
 
 # odczytamy dane z bazy
+
+
 def czytaj_baze():
-	for miary in Calkowite.select():
-		print(miary.data, miary.pomiary)
-	print("")
+    for miary in Calkowite.select():
+        print(miary.data, miary.pomiary)
+        print()
 
 ############################################
-
-
-
 
 
 czytaj_baze()
